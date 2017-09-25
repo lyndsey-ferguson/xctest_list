a737f4e923051cd48c63a2d2a1e845fe6aa19ff6
Lyndsey Ferguson <lyndsey.ferguson@appian.com>
2017-09-25 11:01:01 -0400

    add support for swift tests

--- lib/xctest_list.rb
+++ lib/xctest_list.rb
@@ -1,7 +1,3 @@
-# NSString *swiftNmCmdline = @"nm -gU '%@' | cut -d' ' -f3 | xargs xcrun swift-demangle | cut -d' ' -f3 | grep -e '[\\.|_]'test";
-
-require 'pathname'
-
 class XCTestList
   def self.validate_bundle(xctest_bundle_path)
     raise "Cannot find xctest bundle at path '#{xctest_bundle_path}'" unless Dir.exist?(xctest_bundle_path)
@@ -21,16 +17,35 @@ class XCTestList
     xctest_binary_path
   end
 
-  def self.tests(xctest_bundle_path)
-    objc_symbols = 'nm -U '
-    objc_symbols << "'#{binary_path(xctest_bundle_path)}'"
+  def self.objc_tests(xctest_bundle_path)
+    objc_symbols_cmd = 'nm -U '
+    objc_symbols_cmd << "'#{binary_path(xctest_bundle_path)}'"
 
     tests = []
-    `#{objc_symbols}`.each_line do |line|
+    `#{objc_symbols_cmd}`.each_line do |line|
       if / t -\[(?<testclass>\w+) (?<testmethod>test\w+)\]/ =~ line
         tests << "#{testclass}/#{testmethod}"
       end
     end
     tests
+  end
+
+  def self.swift_symbols(swift_symbols_cmd_output)
+    swift_symbols_cmd_output.gsub(/^.* .* (.*)$/, '\1')
+  end
+
+  def self.swift_tests(xctest_bundle_path)
+    swift_symbols_cmd_output = `nm -gU '#{binary_path(xctest_bundle_path)}'`
+    tests = []
+    swift_symbols(swift_symbols_cmd_output).each_line do |symbol|
+      if /\w+\.(?<testclass>[^\.]+)\.(?<testmethod>test[^\(]+)/ =~ `xcrun swift-demangle #{symbol}`
+        tests << "#{testclass}/#{testmethod}"
+      end
+    end
+    tests
+  end
+
+  def self.tests(xctest_bundle_path)
+    objc_tests(xctest_bundle_path) | swift_tests(xctest_bundle_path)
   end
 end
