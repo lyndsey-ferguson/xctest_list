50fe1041f63ee03684b06e1c62980757dcb58af7
Samuel Lopes <sam@tegus.co>
2020-07-14 11:48:45 -0500

    Add ability to find tests with different prefixes
    Some testing frameworks for Swift / Objective-C use different test prefixes to identify tests. For instance, [Quick](https://github.com/Quick/Quick) uses the `spec` method name prefix instead of `test`. In order to be able to use the test-center fastlane plugin with `Quick`, we need to be able to identify these differently-named tests, and so this commit introduces the ability to override the prefix that XCTestList uses to find tests.

--- lib/xctest_list.rb
+++ lib/xctest_list.rb
@@ -35,48 +35,50 @@ class XCTestList
   end
 
   # find the Objective-C symbols in the bundle's binary
-  def self.objc_tests(xctest_bundle_path)
+  def self.objc_tests(xctest_bundle_path, test_prefix: "test")
     tests = []
     objc_symbols_command_output_tempfile = Tempfile.new(File.basename(xctest_bundle_path) + "objc")
     system("nm -U '#{binary_path(xctest_bundle_path)}' > '#{objc_symbols_command_output_tempfile.path}'")
     tests = []
     File.foreach(objc_symbols_command_output_tempfile.path) do |line|
-      if / t -\[(?<testclass>\w+) (?<testmethod>test\w*)\]/ =~ line
-        tests << "#{testclass}/#{testmethod}"
+      regex = / t -\[(?<testclass>\w+) (?<testmethod>#{test_prefix}\w*)\]/
+      if matches = regex.match(line)
+        tests << "#{matches[:testclass]}/#{matches[:testmethod]}"
       end
     end
     tests
   end
 
   # cleanup the Swift nm output to only provide the swift symbols
-  def self.swift_symbols(swift_symbols_cmd_output)
+  def self.swift_symbols(swift_symbols_cmd_output, test_prefix: "test")
     swift_symbols_cmd_output.gsub(/^.* .* (.*)$/, '\1')
   end
 
-  def self.swift_symbol_dump_command(xctest_binary_path)
+  def self.swift_symbol_dump_command(xctest_binary_path, test_prefix: "test")
     nm_version_string = `nm -version | head -n 1 | sed -E 's/Apple LLVM version ([0-9]+\.[0-9]+\.[0-9]+).*/\\1/'`.chomp
     if Gem::Version.new(nm_version_string) < Gem::Version.new('9.0.0')
       "nm -gU '#{xctest_binary_path}' | cut -d' ' -f3 | xcrun swift-demangle | grep -E '^\\S+\\.test[^.]+\\(\\)$' | sed 's/ () -> ()/()/'"
     else
-      "nm -gU '#{xctest_binary_path}' | cut -d' ' -f3 | xargs -s 131072 xcrun swift-demangle | cut -d' ' -f3 | grep -e '[\\.|_]'test"
+      "nm -gU '#{xctest_binary_path}' | cut -d' ' -f3 | xargs -s 131072 xcrun swift-demangle | cut -d' ' -f3 | grep -e '[\\.|_]'#{test_prefix}"
     end
   end
 
   # find the Swift symbols in the bundle's binary
-  def self.swift_tests(xctest_bundle_path)
+  def self.swift_tests(xctest_bundle_path, test_prefix: "test")
     swift_symbols_command_output_tempfile = Tempfile.new(File.basename(xctest_bundle_path) + "swift")
-    system("#{swift_symbol_dump_command(binary_path(xctest_bundle_path))}  > '#{swift_symbols_command_output_tempfile.path}'")
+    system("#{swift_symbol_dump_command(binary_path(xctest_bundle_path), test_prefix: test_prefix)}  > '#{swift_symbols_command_output_tempfile.path}'")
     tests = []
     File.foreach(swift_symbols_command_output_tempfile.path) do |line|
-      if /.*-\[.*\]/ !~ line && /\w+\.(?<testclass>[^\.]+)\.(?<testmethod>test[^\(]*)\(/ =~ line
-        tests << "#{testclass}/#{testmethod}"
+      regex = /\w+\.(?<testclass>[^\.]+)\.(?<testmethod>#{test_prefix}[^\(]*)\(/
+      if /.*-\[.*\]/ !~ line && matches = regex.match(line)
+        tests << "#{matches[:testclass]}/#{matches[:testmethod]}"
       end
     end
     tests
   end
 
   # find the Objective-C and Swift tests in the binary's bundle
-  def self.tests(xctest_bundle_path)
-    objc_tests(xctest_bundle_path) | swift_tests(xctest_bundle_path)
+  def self.tests(xctest_bundle_path, test_prefix: 'test')
+    objc_tests(xctest_bundle_path, test_prefix: test_prefix) | swift_tests(xctest_bundle_path, test_prefix: test_prefix)
   end
 end
